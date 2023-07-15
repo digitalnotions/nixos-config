49471c65373366716bba242129a9524c94e471d0
Mark Wood <mark@markandkc.net>
2023-07-15 19:22:42 -0400

    Remove dotfiles and clone repo

--- modules/editors/emacs/default.nix
+++ modules/editors/emacs/default.nix
@@ -2,33 +2,35 @@
 # Personal Emacs configuration
 #
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   services.emacs = {
     enable = true;
-#    package = pkgs.emacs29;
     package = pkgs.emacs-git;
   };
 
   programs.emacs = {
     enable = true;
-#    package = pkgs.emacs29;
     package = pkgs.emacs-git;
   };
 
   home = {
     sessionVariables = {
-#      EDITOR = "${pkgs.emacs-git}/bin/emacsclient -t";
-#      VISUAL = "${pkgs.emacs-git}/bin/emacsclient -c";
       EDITOR = "emacsclient -t";
       VISUAL = "emacsclient -c";
 
     };
-    # TODO: MOVE EMACS CONFIG TO SEPARATE GIT REPO
-    file.".emacs.d" = {
-      source = ./.emacs.d;
-      recursive = true;
-    };
   };
+
+  home.activation = {
+    emacsActivation = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      CONFIG="$HOME/.emacs.d"
+
+      if [ ! -d "$CONFIG" ]; then
+        git clone git@github.com:digitalnotions/emacs.d.git $CONFIG
+      fi
+    '';
+  };
+
 }
