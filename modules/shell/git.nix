d841b6c4bcbc9b0cacc1b561547472a3244a1201
Mark Wood <digitalnotions@users.noreply.github.com>
2023-07-17 21:07:18 -0400

    Remove dotfiles and clone repo (#5)

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -8,6 +8,10 @@
       enable = true;
       userName = "Mark Wood";
       userEmail = "mark@markandkc.net";
+      aliases = {
+        amend = "commit --amend --no-edit";
+        update = "commit --amend -m";
+      };
     };
   };
 }
--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -10,11 +10,17 @@
 
       shellAliases = {
         ll = "ls -al";
+
+        # Emacs
         ec = "${pkgs.emacs-git}/bin/emacsclient -n -c";
+        emacs_restart = "systemctl restart --user emacs && systemctl status --user emacs";
+
+        # NixOS
         nixos_update = "pushd ~/nixconfig && sudo nix flake update && sudo nix flake lock && popd";
         nixos_upgrade = "pushd ~/nixconfig && nixos-rebuild --use-remote-sudo switch --verbose --flake .# && popd && nixos_diff";
         nixos_diff = "nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2)";
         nixos_gc = "sudo nix-collect-garbage -d && nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+
 
         # Always enable color for grep
         grep = "grep --color=auto";
