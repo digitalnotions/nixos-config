655bf3251050b511fd7d4983b30102feddcbc4e6
Mark Wood <mark@markandkc.net>
2025-03-15 20:32:09 -0400

    Remove block hosts

--- system/modules/blockhosts.nix
+++ system/modules/blockhosts.nix
@@ -2,11 +2,6 @@
 
 {
   networking.extraHosts = ''
-  0.0.0.0    youtube.com
-  0.0.0.0    fandom.com
-  0.0.0.0    minecraft.wiki
-  0.0.0.0    minecraft.net
-  0.0.0.0    planetminecraft.com
   '';
   
 }
